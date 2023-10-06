064ea4c18405e69928b1f6442e8c4236beffefba
Dylan Gresham <98201122+Dylan-Gresham@users.noreply.github.com>
2023-10-06 11:33:16 -0600

    Begin creating PersonalInfo component--Progress Commit

--- src/PersonalInfo.jsx
+++ src/PersonalInfo.jsx
@@ -1,7 +1,26 @@
 export default function PersonalInfo() {
 	return (
 		<div className="personalInfoContainer">
-			{/* Insert labels/inputs for first/last/phone# (optional) /email/fax (optional) */}
+			<div className="inputContainer">
+				<label>First Name:  </label>
+				<input placeholder="John"></input>
+			</div>
+			<div className="inputContainer">
+				<label>Last Name:  </label>
+				<input placeholder="Doe"></input>
+			</div>
+			<div className="inputContainer">
+				<label>Phone Number:  </label>
+				<input placeholder="123-456-7890"></input>
+			</div>
+			<div className="inputContainer">
+				<label>Email:  </label>
+				<input placeholder="john.doe@gmail.com"></input>
+			</div>
+			<div className="inputContainer">
+				<label>Fax:  </label>
+				<input placeholder="+1-123-456-7890"></input>
+			</div>
 		</div>
 	);
 }
