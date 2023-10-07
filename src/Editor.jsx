37f641e344a007674daaa9829051e2b819972162
Dylan Gresham <98201122+Dylan-Gresham@users.noreply.github.com>
2023-10-07 14:52:20 -0600

    Create states for Education & WorkExperience

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -5,17 +5,26 @@ import WorkExperience from "./WorkExperience.jsx";
 
 export default function Editor() {
 	const [previewPersonalInputs, setPreviewPersonalInputs] = useState({firstName: '', lastName: '', phone: '', email: '', fax: ''});
+	const [previewEducationInputs, setPreviewEduactionInputs] = useState({schoolName: '', major: '', startDate: '', endDate: ''});
+	const [previewWorkExperienceInputs, setPreviewWorkExperienceInputs] = useState({company: '', position: '', reponsibilities: [], startDate: '', endDate: ''});
 
 	function changePreviewPersonalInputs(childProps) {
-		console.log(childProps);
 		setPreviewPersonalInputs({...previewPersonalInputs, firstName: childProps.firstName, lastName: childProps.lastName, phone: childProps.phone, email: childProps.email, fax: childProps.fax});
+	}
+
+	function changePreviewEducationInputs(childProps) {
+		setPreviewEduactionInputs({...previewEducationInputs, schoolName: childProps.schoolName, major: childProps.major, startDate: childProps.startDate, endDate: childProps.endDate});
+	}
+
+	function changePreviewWorkExperienceInputs(childProps) {
+		setPreviewWorkExperienceInputs({...previewWorkExperienceInputs, company: childProps.company, position: childProps.position, responsibilities: childProps.responsibilities, startDate: childProps.startDate, endDate: childProps.endDate});
 	}
 
 	return (
 		<div className="editor">
 			<PersonalInfo callback={changePreviewPersonalInputs}/>
-			<Education />
-			<WorkExperience />
+			<Education callback={changePreviewEducationInputs}/>
+			<WorkExperience callback={changePreviewWorkExperienceInputs}/>
 		</div>
 	);
 }
