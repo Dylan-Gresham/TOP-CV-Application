4e52f85d84aabf7fb4bed674142776358634c6b5
Dylan Gresham <98201122+Dylan-Gresham@users.noreply.github.com>
2023-10-07 14:44:40 -0600

    Implement PersonalInfo inputs & back prop-ing

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import PersonalInfo from "./PersonalInfo.jsx";
 import Education from "./Education.jsx";
 import WorkExperience from "./WorkExperience.jsx";
 
 export default function Editor() {
+	const [previewPersonalInputs, setPreviewPersonalInputs] = useState({firstName: '', lastName: '', phone: '', email: '', fax: ''});
+
+	function changePreviewPersonalInputs(childProps) {
+		console.log(childProps);
+		setPreviewPersonalInputs({...previewPersonalInputs, firstName: childProps.firstName, lastName: childProps.lastName, phone: childProps.phone, email: childProps.email, fax: childProps.fax});
+	}
+
 	return (
 		<div className="editor">
-			<PersonalInfo />
+			<PersonalInfo callback={changePreviewPersonalInputs}/>
 			<Education />
 			<WorkExperience />
 		</div>
--- src/PersonalInfo.jsx
+++ src/PersonalInfo.jsx
@@ -1,26 +1,54 @@
-export default function PersonalInfo() {
+import { useState } from "react";
+
+export default function PersonalInfo({callback}) {
+	const [inputs, setInputs] = useState({firstName: "John", lastName: "Doe", phone: "123-456-7890", email: "john.doe@gmail.com", fax: "+1-123-456-7890"});
+	
+	function handleChange(designator, info) {
+		if(designator === 0) {
+			setInputs({...inputs, firstName: `${info}`});
+		} else if(designator === 1) {
+			setInputs({...inputs, lastName: `${info}`});
+		} else if(designator === 2) {
+			setInputs({...inputs, phone: `${info}`});
+		} else if(designator === 3) {
+			setInputs({...inputs, email: `${info}`});
+		} else if(designator === 4) {
+			setInputs({...inputs, fax: `${info}`});
+		}
+	}
+
+	function callCallback() {
+		callback(inputs);
+	}
+
 	return (
 		<div className="personalInfoContainer">
 			<div className="inputContainer">
-				<label>First Name:  </label>
-				<input placeholder="John"></input>
+				<label className="inputLabel" htmlFor="firstNameInput">First Name:  </label>
+				<input className="personalInfoInput" id="firstNameInput" placeholder={inputs.firstName} 
+					onChange={(event) => handleChange(0, event.target.value)}></input>
 			</div>
 			<div className="inputContainer">
-				<label>Last Name:  </label>
-				<input placeholder="Doe"></input>
+				<label className="inputLabel" htmlFor="lastNameInput">Last Name:  </label>
+				<input className="personalInfoInput" id="lastNameInput" placeholder={inputs.lastName} 
+					onChange={(event) => handleChange(1, event.target.value)}></input>
 			</div>
 			<div className="inputContainer">
-				<label>Phone Number:  </label>
-				<input placeholder="123-456-7890"></input>
+				<label className="inputLabel" htmlFor="phoneNumberInput">Phone Number:  </label>
+				<input className="personalInfoInput" id="phoneNumberInput" placeholder={inputs.phone} 
+					onChange={(event) => handleChange(2, event.target.value)}></input>
 			</div>
 			<div className="inputContainer">
-				<label>Email:  </label>
-				<input placeholder="john.doe@gmail.com"></input>
+				<label className="inputLabel" htmlFor="emailInput">Email:  </label>
+				<input className="personalInfoInput" id="emailInput" placeholder={inputs.email} 
+					onChange={(event) => handleChange(3, event.target.value)}></input>
 			</div>
 			<div className="inputContainer">
-				<label>Fax:  </label>
-				<input placeholder="+1-123-456-7890"></input>
+				<label className="inputLabel" htmlFor="faxInput">Fax (optional):  </label>
+				<input className="personalInfoInput" id="faxInput" placeholder={inputs.fax} 
+					onChange={(event) => handleChange(4, event.target.value)}></input>
 			</div>
+			<button type="button" onClick={callCallback}>Submit</button>
 		</div>
 	);
 }
