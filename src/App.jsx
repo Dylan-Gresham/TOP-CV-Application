ad19635d2ff4d51256c654fa4d37f740fd21ea44
Dylan Gresham <98201122+Dylan-Gresham@users.noreply.github.com>
2023-10-10 10:33:38 -0600

    Lift state from Editor.jsx to App.jsx
    Lifted once again to be able to pass state to the `Preview.jsx` component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,52 @@
-import { useState } from 'react';
 import Header from "./Header.jsx";
-import Editor from "./Editor.jsx";
-import Preview from "./Preview.jsx";
 import Footer from "./Footer.jsx";
+import Preview from "./Preview.jsx";
+import Education from "./Education.jsx";
+import PersonalInfo from "./PersonalInfo.jsx";
+import WorkExperience from "./WorkExperience.jsx";
+import {useState} from "react";
 import './App.css'
 
 function App() {
+	const inputs = {
+		firstName: '',
+		lastName: '',
+		phone: '',
+		email: '',
+		fax: '',
+		schoolName: '',
+		major: '',
+		sStartDate: '',
+		sEndDate: '',
+		company: '',
+		position: '',
+		responsibilities: [],
+		startDate: '',
+		endDate: ''
+	};
+	const [previewInputs, setPreviewInputs] = useState(inputs);
+
+	function handlePersonalInfoChange(childProps) {
+		setPreviewInputs({...previewInputs, firstName: childProps.firstName, lastName: childProps.lastName, phone: childProps.phone, email: childProps.email, fax: childProps.fax});
+	}
+
+	function handleEducationChange(childProps) {
+		setPreviewInputs({...previewInputs, schoolName: childProps.schoolName, major: childProps.major, sStartDate: childProps.startDate, sEndDate: childProps.endDate});
+	}
+
+	function handleWorkExperienceChange(childProps) {
+		setPreviewInputs({...previewInputs, company: childProps.company, position: childProps.position, responsibilities: childProps.responsibilities, startDate: childProps.startDate, endDate: childProps.endDate});
+	}
+
+	console.log(previewInputs);
+
 	return (
 		<>
 			<Header />
-			<Editor />
-			<Preview />
+			<PersonalInfo callback={handlePersonalInfoChange} />
+			<Education callback={handleEducationChange} />
+			<WorkExperience callback={handleWorkExperienceChange} />
+			<Preview props={previewInputs} />
 			<Footer />
 		</>
 	);
--- src/Preview.jsx
+++ src/Preview.jsx
@@ -1,4 +1,4 @@
-export default function Preview() {
+export default function Preview(props) {
 	return (
 		<div className="previewContainer">
 			{/* Insert a preview of the filled out resume */}
